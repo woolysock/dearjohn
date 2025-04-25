ed666f1e9eacae5128def999e5de4dba97680358
Megan Donahue <megang@gmail.com>
2025-04-24 18:54:46 -0700

    Initial Commit

---
+++ why/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  why
+//
+//  Created by Megan Donahue on 4/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
